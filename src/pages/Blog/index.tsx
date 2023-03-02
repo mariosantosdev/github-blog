775c484601f919dcf20e3e086846be58d1b96b73
Mário Santos <mariodev7@gmail.com>
2023-03-02 20:45:30 -0300

    feat(blog-page): using posts from api

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -2,10 +2,12 @@ import { Fragment } from 'react'
 import { PostCard } from '~/components/PostCard'
 import { Profile } from '~/components/Profile'
 import { SearchBar } from '~/components/SearchBar'
+import { usePosts } from '~/contexts/PostsContext'
 
 import { Content, PostsList } from './styles'
 
 export function BlogPage() {
+  const { posts } = usePosts()
   return (
     <Fragment>
       <Profile />
@@ -14,12 +16,9 @@ export function BlogPage() {
 
       <Content>
         <PostsList>
-          <PostCard id={1} />
-          <PostCard id={2} />
-          <PostCard id={3} />
-          <PostCard id={4} />
-          <PostCard id={5} />
-          <PostCard id={6} />
+          {posts.map((post) => (
+            <PostCard key={post.id} {...post} />
+          ))}
         </PostsList>
       </Content>
     </Fragment>
